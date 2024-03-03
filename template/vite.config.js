248ec277b27a6edd8665edb8308cb513bbb76536
David Prieto Recio <dam.david.prieto@gmail.com>
2024-03-03 21:26:43 +0100

    añadir vite config

---
+++ template/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite"
+
+export default () => {
+    return defineConfig({
+        root:'src',
+        resolve:{
+            alias:{
+                '@': resolve(__dirname,'/src'),
+                '@assets': resolve(__dirname,'/src/assets'),
+                '@components': resolve(__dirname,'/src/components'),
+                '@commons': resolve(__dirname,'/src/components/commons'),
+                '@composes': resolve(__dirname,'/src/components/composes'),
+                '@views': resolve(__dirname,'/src/views'),
+                '@layouts': resolve(__dirname,'/src/layouts'),
+                '@styles': resolve(__dirname,'/src/styles')
+            }
+        },
+        plugins: [
+            Inspect()
+        ],
+        build:{
+            outDir: resolve(__dirname,'/dist'),
+            rollupOptions:{
+                input:resolve(__dirname,'/src/index.html')
+            }
+        }
+    })
+}
