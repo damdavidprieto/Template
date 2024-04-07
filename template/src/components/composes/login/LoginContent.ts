0d45c1c7a24dfd5c941ced5e9a4c13ab3c00cd0c
David Prieto Recio <dam.david.prieto@gmail.com>
2024-04-07 14:25:03 +0200

    ejemplo de card en columna y fila

--- template/src/components/composes/login/LoginContent.ts
+++ template/src/components/composes/login/LoginContent.ts
@@ -13,22 +13,47 @@ export class LoginContent extends LitElement {
         `
     ];
 
+    _renderCardLoginColumn(){
+        return html`
+          <app-card titleCard="Login" class="m-auto">
+            <app-container>
+              <app-home-icon title="tooltip de prueba" size="200"></app-home-icon>
+            </app-container>
+            <app-form>
+              <app-form-row>
+                <app-form-control class="bold" placeholder="Usuario" type="text">Usuario:</app-form-control>
+                <app-form-control class="bold" placeholder="Contraseña" type="password">Contraseña:</app-form-control>
+                <app-form-control placeholder="Contraseña" type="checkbox">Recordarme</app-form-control>
+              </app-form-row>
+              <app-button class="success bold">Login</app-button>
+            </app-form>
+          </app-card> 
+        `;
+    }
+
+    _renderCardLoginRow(){
+        return html`
+          <app-card titleCard="Login" class="m-auto row">
+            <app-container>
+              <app-home-icon title="tooltip de prueba" size="200"></app-home-icon>
+            </app-container>
+            <app-form>
+              <app-form-row>
+                <app-form-control class="bold" placeholder="Usuario" type="text">Usuario:</app-form-control>
+                <app-form-control class="bold" placeholder="Contraseña" type="password">Contraseña:</app-form-control>
+                <app-form-control placeholder="Contraseña" type="checkbox">Recordarme</app-form-control>
+              </app-form-row>
+              <app-button class="success bold">Login</app-button>
+            </app-form>
+          </app-card> 
+        `;
+    }
+
     render() {
         return html`
                 <app-container>
-                  <app-card titleCard="Login" class="m-auto">
-                    <app-container>
-                      <app-home-icon title="tooltip de prueba" size="200"></app-home-icon>
-                    </app-container>
-                    <app-form>
-                      <app-form-row>
-                        <app-form-control class="bold" placeholder="Usuario" type="text">Usuario:</app-form-control>
-                        <app-form-control class="bold" placeholder="Contraseña" type="password">Contraseña:</app-form-control>
-                        <app-form-control placeholder="Contraseña" type="checkbox">Recordarme</app-form-control>
-                      </app-form-row>
-                      <app-button class="success bold">Login</app-button>
-                    </app-form>
-                  </app-card>                  
+                  ${this._renderCardLoginColumn()} 
+                  ${this._renderCardLoginRow()}               
                 </app-container> 
             `;
     }
